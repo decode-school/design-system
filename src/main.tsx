70dc41cadcd0d333625287045906124e5ab64c18
Sébastien Gaudard <43199628+SebGrd@users.noreply.github.com>
2025-01-29 20:41:51 +0100

    feat: added icon props + icon display to input element

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,20 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <main className='bg-secondary'>
       <div className='container mx-auto p-4 space-y-4'>
-        <h1>Décode Design System</h1>
-        <Button>Click me</Button>
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
-        <Button variant="outline">Outline</Button>
+        <h1 className='text-4xl mb-16'>Décode Design System</h1>
+        <section className='flex gap-4 flex-wrap'>
+          <Button>Click me</Button>
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
+          <Button variant="outline">Outline</Button>
+        </section>
         <hr />
         <Input type="text" placeholder="Recherche" />
+        <Input type="search" placeholder="Recherche" icon="search" />
+        <hr />
       </div>
     </main>
   </StrictMode>,
--- src/package/ui/components/ui/input.tsx
+++ src/package/ui/components/ui/input.tsx
@@ -1,40 +1,40 @@
 import * as React from "react"
-import { Search } from "lucide-react"
-
-import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/package/ui/lib/utils"
+import { DynamicIcon, IconName } from "lucide-react/dynamic"
 
-const inputVariants = cva(
-  "flex h-10 w-full rounded-lg bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground placeholder:font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-  {
-    variants: {
-      type: {
-        search: "pl-10",
-      },
-    },
-    defaultVariants: {
-      type: "search"
-    },
-  });
-
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> { }
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: IconName
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "search", ...props }, ref) => {
+  ({ className, type, icon, ...props }, ref) => {
+    if (icon) {
+      return (
+        <div className="relative w-full">
+          <DynamicIcon name={icon} className="text-muted-foreground absolute left-2 top-1/2 -translate-y-1/2" />
+          <input
+            type={type}
+            className={cn(
+              "flex h-10 w-full rounded-lg bg-background px-3 pl-10 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground placeholder:font-bold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+        </div>
+      )
+    }
     return (
-      <div className="relative w-full">
-        <Search className="text-muted-foreground absolute left-2 top-1/2 -translate-y-1/2" />
-        <input
-          type={type}
-          className={cn(
-            inputVariants({ type, className }),
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-lg bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground placeholder:font-bold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
     )
   }
 )
