5db9adc9fc500bcd02499791bc1b79761423c100
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2025-01-29 15:26:18 +0100

    feat: added button + added accent color

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './package/ui/components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <main>
       <h1>Décode Design System</h1>
+      <Button>Click me</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+      <Button variant="outline">Outline</Button>
     </main>
   </StrictMode>,
 )
