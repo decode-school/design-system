fb88d6e54f93b51f39e3c129a27ddcfcd950dae3
Sébastien Gaudard <43199628+SebGrd@users.noreply.github.com>
2025-01-29 22:08:18 +0100

    feat: add select component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/package/ui/components/ui/button'
 import { Input } from '@/package/ui/components/ui/input'
 import { ThemeProvider } from "@/package/ui/providers/theme-provider"
 import ModeToggle from './package/ui/components/functionnal/mode-toggle'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './package/ui/components/ui/select'
 
 
 
@@ -13,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <main>
         <header className='container mx-auto p-4 flex justify-between items-center'>
-          <h1 className='text-4xl'>Décode Design System</h1>
+          <h1 className='text-4xl font-bold'>Décode Design System</h1>
           <ModeToggle lightText='light' darkText='dark' />
         </header>
         <div className='container mx-auto p-4 space-y-4'>
@@ -29,6 +30,18 @@ createRoot(document.getElementById('root')!).render(
           <hr />
           <Input type="text" placeholder="Recherche" />
           <Input type="search" placeholder="Recherche" icon="search" />
+          <hr />
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Choisissez une année" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="2023">2023/2024</SelectItem>
+              <SelectItem value="2024">2024/2025</SelectItem>
+              <SelectItem value="2025">2025/2026</SelectItem>
+              <SelectItem value="2026">2026/2027</SelectItem>
+            </SelectContent>
+          </Select>
           <hr />
         </div>
       </main>
